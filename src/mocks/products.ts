e19ea23c146e07e72ae5644cf368ffd3ac37e073
Parsa361 <parsafarahany821@gmail.com>
2026-05-19 16:54:28 +0330

    Mock Data and API Layer Created

---
+++ src/mocks/products.ts
@@ -0,0 +1,31 @@
+import type { Product } from "../types/product";
+
+export const MOCK_PRODUCTS: Product[] = [
+  {
+    id: 1,
+    title: "iPhone 15",
+    description: "Apple flagship",
+    price: 999,
+    thumbnail: "",
+    category: "smartphones",
+    rating: 4.8,
+  },
+  {
+    id: 2,
+    title: "Samsung S23",
+    description: "Android flagship",
+    price: 899,
+    thumbnail: "",
+    category: "smartphones",
+    rating: 4.7,
+  },
+  {
+    id: 3,
+    title: "MacBook Pro",
+    description: "M3 Chip",
+    price: 1999,
+    thumbnail: "",
+    category: "laptops",
+    rating: 4.9,
+  },
+];
